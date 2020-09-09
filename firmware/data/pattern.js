577e8b592c538863623443d0c82939992f04e928
Space Buck <heyspacebuck@gmail.com>
2020-09-09 21:21:31 +0200

    Implemented patterns for vibration output

---
+++ firmware/data/pattern.js
@@ -0,0 +1,71 @@
+const $patternSelector = document.getElementById("patternSelector");
+const $patternParams = document.getElementById("patternParams");
+
+const createRangeElements = (
+  rangeName,
+  rangeLabel,
+  rangeMin,
+  rangeMax,
+  rangeStep = 1
+) => {
+  const $element = document.createElement("div");
+  const $label = document.createElement("label");
+  $label.htmlFor = rangeName;
+  $label.innerText = rangeLabel;
+  const $range = document.createElement("input");
+  $range.type = "range";
+  $range.id = rangeName;
+  $range.name = rangeName;
+  $range.min = rangeMin;
+  $range.max = rangeMax;
+  $range.step = rangeStep;
+  $range.value = (rangeMax + rangeMin) / 2;
+  const $text = document.createElement("span");
+  $text.innerText = `${$range.value}`;
+  $range.addEventListener("input", () => {
+    $text.innerText = `${$range.value}`;
+  });
+  $element.appendChild($label);
+  $element.appendChild($range);
+  $element.appendChild($text);
+  return $element;
+};
+
+const listParams = () => {
+  $patternParams.innerHTML = "";
+  $patternParams.style.display = "block";
+  if ($patternSelector.value == "pwm") {
+    const $intensity = createRangeElements(
+      "intensity",
+      "Intensity:",
+      0,
+      1,
+      0.01
+    );
+    $patternParams.appendChild($intensity);
+  }
+  if ($patternSelector.value == "sine") {
+    const $maxIntensity = createRangeElements(
+      "maxIntensity",
+      "Max intensity:",
+      0,
+      1,
+      0.01
+    );
+    const $minIntensity = createRangeElements(
+      "minIntensity",
+      "Min intensity:",
+      0,
+      1,
+      0.01
+    );
+    const $period = createRangeElements("period", "Period:", 1, 30, 0.1);
+    $patternParams.appendChild($maxIntensity);
+    $patternParams.appendChild($minIntensity);
+    $patternParams.appendChild($period);
+  }
+};
+
+listParams();
+
+$patternSelector.addEventListener("change", listParams);
